3a72144d019fdb3d3138b3d16a687a607f24bc98
HKinslow <heatherkinslow6@gmail.com>
2019-09-12 16:44:55 -0400

    Worked on prototype-refactor.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,10 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        console.log(`${this.name} says, Hello!`);
+    }
+}
